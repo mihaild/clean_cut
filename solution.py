69377ed91f3524e6cd9ee14df0f39454d1e9294c
Mikhail Dektyarev <mihail.dektyarow@gmail.com>
2016-10-07 00:40:45 +0300

    read param

--- solution.py
+++ solution.py
@@ -1,7 +1,11 @@
 from scipy import misc
 from tqdm import tqdm
+import sys
 
-IMAGE = 'ball.jpg'
+if len(sys.argv) > 1:
+    IMAGE = sys.argv[1]
+else:
+    IMAGE = 'ball.jpg'
 
 T = [0, 255, 0]
 S = [255, 0, 0]
@@ -16,11 +20,11 @@ def get_neighbours(i, j, max_i, max_j):
 	if j > 0:
 		result.append((i, j - 1))
 	if (i + 1) < max_i:
-		result.append((i + 1, j))		
+		result.append((i + 1, j))
 	if (j + 1) < max_j:
 		result.append((i, j  + 1))
 
-	return result	
+	return result
 
 def flip(i, j, A, B):
 	if (i, j) in A:
@@ -40,10 +44,10 @@ def cut(image):
 		for i in tqdm(range(dim_x), total=dim_x):
 			for j in range(dim_y):
 				if (i,j) in A:
-					in_cut =  dist(image[i, j, : 3], S) 
-					in_set =  dist(image[i, j, : 3], T) 
+					in_cut =  dist(image[i, j, : 3], S)
+					in_set =  dist(image[i, j, : 3], T)
 				else:
-					in_set =  dist(image[i, j, : 3], S) 
+					in_set =  dist(image[i, j, : 3], S)
 					in_cut =  dist(image[i, j, : 3], T)
 
 				for i_n, j_n in get_neighbours(i, j, dim_x, dim_y):
@@ -66,7 +70,7 @@ def cut(image):
 if __name__ == "__main__":
 	image = misc.imread(IMAGE)
 	dim_x, dim_y, colors = image.shape
-	print "Loaded image of shape {x}, {y}".format(x=dim_x, y=dim_y) 
+	print "Loaded image of shape {x}, {y}".format(x=dim_x, y=dim_y)
 
 	A, B = cut(image)
 
@@ -77,7 +81,7 @@ if __name__ == "__main__":
 				image[i, j, 1] = image[i, j, 1] / 3
 				image[i, j, 2] = image[i, j, 2] / 3
 			elif (i,j) in B:
-				image[i, j, 0] = image[i, j, 0] / 3 
+				image[i, j, 0] = image[i, j, 0] / 3
 				image[i, j, 1] = min(255, image[i, j, 1] + 100)
 				image[i, j, 2] = image[i, j, 2] / 3
 
